da6ea53c7edfbf9f68a688d133f377a695d46a15
Ermano Cristovão <ercris990@gmail.com>
2020-07-25 00:17:50 +0100

    aletracao teste002

--- teste002.py
+++ teste002.py
@@ -3,10 +3,10 @@ numlist = []
 while True:
     num = int(input('Digite um numero: '))
     opc = str(input('Deseja continuar S|N: ')).strip().upper()
-    if opc not in 'SsNn':
+    numlist.append(num)
+    while opc not in 'SsNn':
         opc = str(
             input('Digite [S] para continuar e [N] para parar: ')).strip().upper()
     if opc in 'Nn':
         break
-    numlist.append(num)
 print(f'Os valores digitados foram: {numlist}')
