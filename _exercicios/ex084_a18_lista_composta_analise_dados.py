d8e5a762956b370eae30becfe9c2c60a8bd25ae5
Ermano Cristovão <ercris990@gmail.com>
2020-07-26 01:23:09 +0100

    add exercicios 84 e 85

--- _exercicios/ex084_a18_lista_composta_analise_dados.py
+++ _exercicios/ex084_a18_lista_composta_analise_dados.py
@@ -5,7 +5,6 @@ print('=-=' * 25)
 while True:
     temp.append(str(input('Nome: ')).strip().title())
     temp.append(float(input('Peso: ')))
-
     if len(principal) == 0:
         maiorpeso = menorpeso = temp[1]
     else:
@@ -13,7 +12,6 @@ while True:
             maiorpeso = temp[1]
         if temp[1] < menorpeso:
             menorpeso = temp[1]
-
     principal.append(temp[:])
     temp.clear()
     opc = str(input('Deseja continuar S/N: ')).strip().capitalize()
